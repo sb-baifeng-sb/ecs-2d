572108dd0193bad374417519d1b564aaef383b7a
baifeng <qq394811161@gmail.com>
2019-07-28 11:30:32 +0800

    fix error

--- build.sh
+++ build.sh
@@ -5,7 +5,16 @@ if [ "$1" = "-all" ]; then
     make
     cd ../..
 fi
+
 mkdir build
+
+if [ "$1" = "-clean" ]; then
+    cd build
+    make clean
+    make
+    cd ..
+fi
+
 cd build
 cmake ..
 make
